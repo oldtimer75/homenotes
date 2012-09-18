e5e18daa062609ed6c7db2eed377192d3f8d5081
Derek Szeto <dszeto@gmail.com>
2012-09-18 14:05:50 -0400

    Initial commit updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homenotes::Application.config.secret_token = 'd569167a40847d899e2e9c52e2117e4695e1a7a80a85ec9ff78dac3cbe6283332f32c788bf3ee955a59c8c852bb9580f8265b68b84e6fd37b08e17a3871d931f'
