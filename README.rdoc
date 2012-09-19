5618a72c42f4f7c94cfdf63d0a8f4a1e1e2fd520
Derek Szeto <dszeto@gmail.com>
2012-09-19 12:38:48 -0400

    Added template for pages

---
+++ README.rdoc
@@ -0,0 +1,1 @@
+This is the HomeNotes web application to help make your house hunting process more organized.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Homenotes::Application.routes.draw do
+  root to: 'static_pages#home'
+
+  match '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
